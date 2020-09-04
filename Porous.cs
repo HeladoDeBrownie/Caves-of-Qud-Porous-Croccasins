d6468def27acf57c0bb0b92ff17e998146094090
helado de brownie <helado.de.brownie@gmail.com>
2020-09-04 02:16:24 -0400

    rehaul and redesign mod

---
+++ Porous.cs
@@ -0,0 +1,49 @@
+namespace XRL.World.Parts
+{
+    [System.Serializable]
+    public class helado_PorousCroccasins_Porous : IPart
+    {
+        public const string POROUS_DESCRIPTION = "Like their namesake, they are scaly and temperamental; unlike their namesake, they are porous and worn on one's feet.";
+        public int Chance = 10;
+
+        public override bool WantEvent(int id, int cascade)
+        {
+            return
+                id == GetDisplayNameEvent.ID ||
+                id == GetShortDescriptionEvent.ID ||
+                id == ObjectCreatedEvent.ID ||
+                base.WantEvent(id, cascade);
+        }
+
+        public override bool HandleEvent(GetDisplayNameEvent @event)
+        {
+            @event.AddAdjective("{{g-K sequence|porous}}");
+            return true;
+        }
+
+        public override bool HandleEvent(GetShortDescriptionEvent @event)
+        {
+            @event.Base.Clear().Append(POROUS_DESCRIPTION);
+            return true;
+        }
+
+        public override bool HandleEvent(ObjectCreatedEvent @event)
+        {
+            AddPlayerMessage("DEBUG: created");
+
+            if (!Chance.in100())
+            {
+                ParentObject.RemovePart(this);
+            }
+
+            return true;
+        }
+
+        public override bool SameAs(IPart part)
+        {
+            return
+                Chance == (part as helado_PorousCroccasins_Porous).Chance &&
+                base.SameAs(part);
+        }
+    }
+}
---
+++ README.markdown
@@ -0,0 +1,1 @@
+Porous Croccasins is a *Caves of Qud* mod that adds the possibility of croccasins coming with holes in them. There is no deliberate mechanical benefit or drawback to this.
